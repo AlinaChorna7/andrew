a635aff8b889db20d3a2fc6ea335fcefb86af947
AlinaChorna7 <159713721+AlinaChorna7@users.noreply.github.com>
2024-10-02 20:27:55 +0200

    add

--- src/components/ToDoList/ToDoList.jsx
+++ src/components/ToDoList/ToDoList.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import styles from './TodoList.module.css'; // Імпортуйте стилі
+import styles from './ToDoList.module.css'; // Імпортуйте стилі
 
 const TodoList = () => {
   const navigate = useNavigate();
